a73f7bed14688f49d7e0d69a9335fdd544e8e229
vale0068 <gabriel.valente@etudiant.univ-reims.fr>
2023-11-15 11:04:27 +0100

    Ajout de l'entite CategorieRecette

---
+++ src/Entity/CategorieRecette.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategorieRecetteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategorieRecetteRepository::class)]
+class CategorieRecette
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $idCatRecette = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nomCatRecette = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdCatRecette(): ?int
+    {
+        return $this->idCatRecette;
+    }
+
+    public function setIdCatRecette(int $idCatRecette): static
+    {
+        $this->idCatRecette = $idCatRecette;
+
+        return $this;
+    }
+
+    public function getNomCatRecette(): ?string
+    {
+        return $this->nomCatRecette;
+    }
+
+    public function setNomCatRecette(string $nomCatRecette): static
+    {
+        $this->nomCatRecette = $nomCatRecette;
+
+        return $this;
+    }
+}
