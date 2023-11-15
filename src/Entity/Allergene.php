8a09203a5f63a0cc0241e582d955ea6846d2a00c
vale0068 <gabriel.valente@etudiant.univ-reims.fr>
2023-11-15 11:01:11 +0100

    Ajout de l'entite Allergene

---
+++ src/Entity/Allergene.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AllergeneRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AllergeneRepository::class)]
+class Allergene
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $idAller = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nomAller = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdAller(): ?int
+    {
+        return $this->idAller;
+    }
+
+    public function setIdAller(int $idAller): static
+    {
+        $this->idAller = $idAller;
+
+        return $this;
+    }
+
+    public function getNomAller(): ?string
+    {
+        return $this->nomAller;
+    }
+
+    public function setNomAller(string $nomAller): static
+    {
+        $this->nomAller = $nomAller;
+
+        return $this;
+    }
+}
