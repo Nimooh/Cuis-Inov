9b33a05812ad09f3ccda86b7c42eacfd9c793db1
burl0005 <brice.burlot@etudiant.univ-reims.fr>
2023-11-19 16:41:31 +0100

    Correction des relations de la base de données

--- src/Entity/CategorieRecette.php
+++ src/Entity/CategorieRecette.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CategorieRecetteRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CategorieRecetteRepository::class)]
@@ -15,6 +17,14 @@ class CategorieRecette
 
     #[ORM\Column(length: 255)]
     private ?string $nomCatRecette = null;
+
+    #[ORM\OneToMany(mappedBy: 'idCatRecette', targetEntity: Recette::class)]
+    private Collection $idRecette;
+
+    public function __construct()
+    {
+        $this->idRecette = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +39,36 @@ class CategorieRecette
     public function setNomCatRecette(string $nomCatRecette): static
     {
         $this->nomCatRecette = $nomCatRecette;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Recette>
+     */
+    public function getIdRecette(): Collection
+    {
+        return $this->idRecette;
+    }
+
+    public function addIdRecette(Recette $idRecette): static
+    {
+        if (!$this->idRecette->contains($idRecette)) {
+            $this->idRecette->add($idRecette);
+            $idRecette->setIdCatRecette($this);
+        }
+
+        return $this;
+    }
+
+    public function removeIdRecette(Recette $idRecette): static
+    {
+        if ($this->idRecette->removeElement($idRecette)) {
+            // set the owning side to null (unless already changed)
+            if ($idRecette->getIdCatRecette() === $this) {
+                $idRecette->setIdCatRecette(null);
+            }
+        }
 
         return $this;
     }
