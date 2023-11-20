1a3803643bdd03112a0d098d04d20f5c003eaa7d
Steven Morlet <steven.morlet@etudiant.univ-reims.fr>
2023-11-20 20:09:28 +0100

    Fix cs de 26 fichiers

--- src/Repository/CategorieRecetteRepository.php
+++ src/Repository/CategorieRecetteRepository.php
@@ -21,28 +21,28 @@ class CategorieRecetteRepository extends ServiceEntityRepository
         parent::__construct($registry, CategorieRecette::class);
     }
 
-//    /**
-//     * @return CategorieRecette[] Returns an array of CategorieRecette objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('c.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    //    /**
+    //     * @return CategorieRecette[] Returns an array of CategorieRecette objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('c')
+    //            ->andWhere('c.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('c.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
 
-//    public function findOneBySomeField($value): ?CategorieRecette
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    public function findOneBySomeField($value): ?CategorieRecette
+    //    {
+    //        return $this->createQueryBuilder('c')
+    //            ->andWhere('c.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
