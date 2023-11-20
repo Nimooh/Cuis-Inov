ebd4d557cacda8035d2fe1ef47ae7a73f4ba1ae6
Steven Morlet <steven.morlet@etudiant.univ-reims.fr>
2023-11-20 20:49:51 +0100

    Ajout Cest de Login Controller pour action login

---
+++ tests/Controller/Login/LoginCest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Controller\Login;
+
+use App\Tests\Support\ControllerTester;
+
+class LoginCest
+{
+    public function _before(ControllerTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ControllerTester $I)
+    {
+    }
+}
