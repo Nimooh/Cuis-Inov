01cf273ab53057ad833d32b2cdfba06bf633f793
Steven Morlet <steven.morlet@etudiant.univ-reims.fr>
2023-11-22 22:00:36 +0100

    Fix CS + Update Fixture Membre : creation 75 membres + Update Factory Membre : non fini

--- src/DataFixtures/MemberFixtures.php
+++ src/DataFixtures/MemberFixtures.php
@@ -2,16 +2,14 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\MembreFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-;
 
 class MemberFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-
-
-        $manager->flush();
+        MembreFactory::createMany(75);
     }
 }
--- src/Factory/MembreFactory.php
+++ src/Factory/MembreFactory.php
@@ -11,24 +11,26 @@ use Zenstruck\Foundry\RepositoryProxy;
 /**
  * @extends ModelFactory<Membre>
  *
- * @method        Membre|Proxy create(array|callable $attributes = [])
- * @method static Membre|Proxy createOne(array $attributes = [])
- * @method static Membre|Proxy find(object|array|mixed $criteria)
- * @method static Membre|Proxy findOrCreate(array $attributes)
- * @method static Membre|Proxy first(string $sortedField = 'id')
- * @method static Membre|Proxy last(string $sortedField = 'id')
- * @method static Membre|Proxy random(array $attributes = [])
- * @method static Membre|Proxy randomOrCreate(array $attributes = [])
+ * @method        Membre|Proxy                     create(array|callable $attributes = [])
+ * @method static Membre|Proxy                     createOne(array $attributes = [])
+ * @method static Membre|Proxy                     find(object|array|mixed $criteria)
+ * @method static Membre|Proxy                     findOrCreate(array $attributes)
+ * @method static Membre|Proxy                     first(string $sortedField = 'id')
+ * @method static Membre|Proxy                     last(string $sortedField = 'id')
+ * @method static Membre|Proxy                     random(array $attributes = [])
+ * @method static Membre|Proxy                     randomOrCreate(array $attributes = [])
  * @method static MembreRepository|RepositoryProxy repository()
- * @method static Membre[]|Proxy[] all()
- * @method static Membre[]|Proxy[] createMany(int $number, array|callable $attributes = [])
- * @method static Membre[]|Proxy[] createSequence(iterable|callable $sequence)
- * @method static Membre[]|Proxy[] findBy(array $attributes)
- * @method static Membre[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
- * @method static Membre[]|Proxy[] randomSet(int $number, array $attributes = [])
+ * @method static Membre[]|Proxy[]                 all()
+ * @method static Membre[]|Proxy[]                 createMany(int $number, array|callable $attributes = [])
+ * @method static Membre[]|Proxy[]                 createSequence(iterable|callable $sequence)
+ * @method static Membre[]|Proxy[]                 findBy(array $attributes)
+ * @method static Membre[]|Proxy[]                 randomRange(int $min, int $max, array $attributes = [])
+ * @method static Membre[]|Proxy[]                 randomSet(int $number, array $attributes = [])
  */
 final class MembreFactory extends ModelFactory
 {
+    private \Transliterator $trans;
+
     /**
      * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
      *
@@ -37,6 +39,7 @@ final class MembreFactory extends ModelFactory
     public function __construct()
     {
         parent::__construct();
+        $this->trans = transliterator_create('Any-Lower; Latin-ASCII');
     }
 
     /**
@@ -46,6 +49,12 @@ final class MembreFactory extends ModelFactory
      */
     protected function getDefaults(): array
     {
+        $email = $this->normalizeName($prnm_membre).'.'.$this->normalizeName($nom_membre).'@'.self::faker()->domainName();
+        $roles = ['ROLE_USER'];
+        $password = self::faker()->password(8);
+        $nom_membre = self::faker()->lastName();
+        $prnm_membre = self::faker()->firstName();
+
         return [
             'email' => 'test@test.com',
             'nomMembre' => 'NomTest',
@@ -53,6 +62,15 @@ final class MembreFactory extends ModelFactory
             'prnmMembre' => 'PrenomTest',
             'roles' => [''],
         ];
+    }
+
+    /**
+     * @param string $nom
+     * @return string
+     */
+    protected function normalizeName(string $nom): string
+    {
+        return preg_replace('/[^a-zA-Z0-9_ ]/', '-', $this->trans->transliterate($nom));
     }
 
     /**
