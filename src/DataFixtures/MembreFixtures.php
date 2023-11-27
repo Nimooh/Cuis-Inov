0450582347775b5a411f97d484423301be7554d9
Steven Morlet <steven.morlet@etudiant.univ-reims.fr>
2023-11-27 20:25:38 +0100

    Update MembreFixtures : replace Member by Membre

--- src/DataFixtures/MembreFixtures.php
+++ src/DataFixtures/MembreFixtures.php
@@ -5,7 +5,7 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
-class MemberFixtures extends Fixture
+class MembreFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
