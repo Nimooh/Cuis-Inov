7d1696eb38832b0ca819e5f2c7baa6e886911776
burl0005 <brice.burlot@etudiant.univ-reims.fr>
2023-11-27 10:43:38 +0100

    implementation de la page Détails

--- src/Controller/DetailsController.php
+++ src/Controller/DetailsController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Repository\RecetteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DetailsController extends AbstractController
 {
     #[Route('/details', name: 'app_details')]
-    public function index(): Response
+    public function index(RecetteRepository $rep, Request $request): Response
     {
+        $id = $request->get('RecetteId');
+        $recipe = $rep->findRecipeById($id);
+        $components = $rep->findAllComponentById($id);
         return $this->render('details/index.html.twig', [
-            'controller_name' => 'DetailsController',
+            'recipe' => $recipe,
+            'components' => $components
         ]);
     }
 }
