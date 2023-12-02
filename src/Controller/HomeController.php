805131e4deb986c07b58e562359a4de7dfc17167
vale0068 <gabriel.valente@etudiant.univ-reims.fr>
2023-12-02 19:54:29 +0100

    Format d'image non-fonctionnel

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\RecetteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(RecetteRepository $repo): Response
     {
-        return $this->render('home/index.html.twig');
+        #$trending = $repo->findOneBy([stars_recette]);
+        #$recettes = $repo->findAll([],['stars_recette' => 'ASC', 'nom_recette' => 'ASC']);
+        $recettes = $repo->find(1);
+        $img = $recettes->getImgRecette();
+
+        #$imgEncoded = "data:image/png;base64," . base64_encode($img);
+        #var_dump($img);
+        return $this->render('home/index.html.twig', ['recettes' => $recettes, 'imgEncoded' => $img]);
     }
 }
