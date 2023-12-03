03e429fc74544b4fe5f5e5f310105291298eafd8
vale0068 <gabriel.valente@etudiant.univ-reims.fr>
2023-12-03 15:52:06 +0100

    Ajout d'une route pour afficher les imgRecette + fix des parametres pour la page d'accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,7 +4,10 @@ namespace App\Controller;
 
 use App\Repository\RecetteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\HeaderUtils;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -12,13 +15,20 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(RecetteRepository $repo): Response
     {
-        #$trending = $repo->findOneBy([stars_recette]);
-        #$recettes = $repo->findAll([],['stars_recette' => 'ASC', 'nom_recette' => 'ASC']);
-        $recettes = $repo->find(1);
-        $img = $recettes->getImgRecette();
+        $trending = $repo->findMostTrending()[0];
+        $recettes = $repo->findAll();
 
-        #$imgEncoded = "data:image/png;base64," . base64_encode($img);
-        #var_dump($img);
-        return $this->render('home/index.html.twig', ['recettes' => $recettes, 'imgEncoded' => $img]);
+        return $this->render('home/index.html.twig', ['recettes' => $recettes, 'trending' => $trending]);
+    }
+
+
+    #[Route("/image/{id}", name: 'app_show_image')]
+    public function showImage(RecetteRepository $repo, int $id): Response
+    {
+        return new Response(
+            $repo->findImgFromId($id)[0]["img_recette"],
+            Response::HTTP_OK,
+            ['content-type' => 'image/png']
+        );
     }
 }
