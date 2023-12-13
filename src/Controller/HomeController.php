856b2a1c47e719ae87b48f67429889e1696f20e6
vale0068 <gabriel.valente@etudiant.univ-reims.fr>
2023-12-13 10:55:58 +0100

    Modification des requêtes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,7 +6,6 @@ use App\Repository\RecetteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Requirement\Requirement;
 
 class HomeController extends AbstractController
 {
@@ -14,19 +13,8 @@ class HomeController extends AbstractController
     public function index(RecetteRepository $repo): Response
     {
         $trending = $repo->findMostTrending()[0];
-        $recettes = $repo->findAllOrderedWithoutMostTrending();
+        $recettes = $repo->findBy([], ['noteMoyenne' => 'DESC', 'nomRecette' => 'ASC']);
 
         return $this->render('home/index.html.twig', ['recettes' => $recettes, 'trending' => $trending]);
-    }
-
-
-    #[Route("/image/{id}", name: 'app_show_image', requirements: ['id' => Requirement::DIGITS])]
-    public function showImage(RecetteRepository $repo, int $id): Response
-    {
-        return new Response(
-            $repo->findImgFromId($id)[0]["img_recette"],
-            Response::HTTP_OK,
-            ['content-type' => 'image/png']
-        );
     }
 }
