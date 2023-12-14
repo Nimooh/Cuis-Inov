818c1fd20e035a4a6a41f97cccb8224e6c77e008
Antoine Terrier <antoine.terrier@etudiant.univ-reims.fr>
2023-12-14 19:05:37 +0100

    Modification du controlleur Dashboard EasyAdmin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,8 +2,15 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Allergene;
+use App\Entity\CategorieRecette;
+use App\Entity\Ingredient;
 use App\Entity\Membre;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
+use App\Entity\Recette;
+use App\Entity\Unite;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -28,5 +35,16 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Membre', 'fas fa-user', Membre::class);
+        yield MenuItem::linkToCrud('Unité', 'fas fa-eye-dropper', Unite::class);
+        yield MenuItem::linkToCrud('Ingrédient', 'fas fa-egg', Ingredient::class);
+        yield MenuItem::linkToCrud('Allergène', 'fas fa-wheat-awn-circle-exclamation', Allergene::class);
+        yield MenuItem::linkToCrud('Recette', 'fas fa-book', Recette::class);
+        yield MenuItem::linkToCrud('Catégorie de recette', 'fas fa-layer-group', CategorieRecette::class);
+    }
+
+    public function configureActions(): Actions
+    {
+        return parent::configureActions()
+            ->add(Crud::PAGE_INDEX, Action::DETAIL);
     }
 }
