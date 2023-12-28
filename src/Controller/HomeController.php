e2bbdd6cb49cd8a3aef77bc5060f98eb3598e4f5
vale0068 <gabriel.valente@etudiant.univ-reims.fr>
2023-12-28 23:56:07 +0100

    Ajout de la route updateDB()

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Repository\InteragirRepository;
 use App\Repository\RecetteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -18,5 +20,21 @@ class HomeController extends AbstractController
         $recettes = $repo->findAllOrderedWithoutMostTrending($trendingId);
 
         return $this->render('home/index.html.twig', ['recettes' => $recettes, 'trending' => $trending]);
+    }
+
+    #[Route('/update', name: 'app_recette_update')]
+    public function updateDB(InteragirRepository $repo, #[MapQueryParameter(filter: \FILTER_VALIDATE_BOOLEAN)] bool $fav,
+                             #[MapQueryParameter(filter: \FILTER_VALIDATE_INT)] int $idRecette)
+    {
+        /** @var \App\Entity\Membre $user */
+        $user = $this->getUser();
+
+        //Redirection vers la page de connexion, si l'utilisateur non-connecté se retrouve sur cette page
+        if($user === null)
+            return $this->redirectToRoute('app_login', [], 303);
+
+        $repo->updateDB($fav, $user->getId(), $idRecette);
+
+        return $this->redirectToRoute('app_home');
     }
 }
