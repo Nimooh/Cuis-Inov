87d510b1229beaae0983d5a461aefd850c7d2fd2
burl0005 <brice.burlot@etudiant.univ-reims.fr>
2023-12-29 17:08:38 +0100

    Nouvelle migration

--- migrations/Version20231229160726.php
+++ migrations/Version20231229160726.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231209134506 extends AbstractMigration
+final class Version20231229160726 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,12 @@ final class Version20231209134506 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE membre DROP img_profil_membre');
-        $this->addSql('ALTER TABLE recette DROP img_recette, CHANGE instruction instruction LONGTEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE interagir CHANGE note_recette note_recette INT DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE membre ADD img_profil_membre LONGBLOB DEFAULT NULL');
-        $this->addSql('ALTER TABLE recette ADD img_recette LONGBLOB DEFAULT NULL, CHANGE instruction instruction VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE interagir CHANGE note_recette note_recette INT NOT NULL');
     }
 }
