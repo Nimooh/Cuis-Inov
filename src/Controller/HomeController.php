77008a273cc337910f0747310b696a410e331047
vale0068 <gabriel.valente@etudiant.univ-reims.fr>
2024-01-03 22:04:41 +0100

    Gestion du trending vide

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -19,10 +19,12 @@ class HomeController extends AbstractController
         $userId = $user ? $user->getId() : 0; //User par défault id 0
 
         $trending = $repo->findMostTrending();
-
-        $trendingId = $trending->getId();
-        $recettes = $repo->findAllOrderedWithoutMostTrending($trendingId, $userId);
-        //dump($recettes);
+        if(!empty($trending)) {
+            $trendingId = $trending->getId();
+            $recettes = $repo->findAllOrderedWithoutMostTrending($trendingId, $userId);
+        } else {
+            $recettes = $repo->findAllOrderedWithoutMostTrending(0, $userId);
+        }
 
         return $this->render('home/index.html.twig', ['recettes' => $recettes, 'trending' => $trending]);
     }
