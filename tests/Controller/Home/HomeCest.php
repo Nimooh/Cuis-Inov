87d89188dafe4f84b664a291107e9857b8692fea
vale0068 <gabriel.valente@etudiant.univ-reims.fr>
2024-01-03 22:34:21 +0100

    Tests sur les rendus (vide ou non)

--- tests/Controller/Home/HomeCest.php
+++ tests/Controller/Home/HomeCest.php
@@ -15,74 +15,22 @@ class HomeCest
         $I->seeInTitle('Cuis\'inov');
     }
 
-    public function areElementGenerated(ControllerTester $I): void
+    public function alternativeRenderingWhenDbIsEmpty(ControllerTester $I): void
     {
         $I->amOnPage('/');
-        $I->seeElement('#trending');
-        $I->see('Les mieux notées :', 'h1');
-        $I->seeElement('#bloc_list');
+        $I->seeResponseCodeIsSuccessful();
+        $I->see('Le site est actuellement indisponible.', 'p:first-of-type');
+        $I->see('Veuillez réessayer ultérieurement.', 'p:last-of-type');
     }
 
-    public function timesOfLiInListIsCorrect(ControllerTester $I): void
+    public function areElementGenerated(ControllerTester $I): void
     {
         RecetteFactory::createMany(15);
 
         $I->amOnPage('/');
-        $I->seeNumberOfElements('#trending', 1);
-        $I->seeNumberOfElements('li', 14);
-    }
-
-    public function isLinkCorrect(ControllerTester $I): void
-    {
-        RecetteFactory::createOne(
-            ['nomRecette' => 'Flanc',
-                'tempsRecette' => new \DateTime('02:36:00'),
-                'diffRecette' => 3,
-                'instruction' => 'Le Lorem Ipsum est un texte employé dans la composition et la mise en page.',
-                'description' => 'Une recette test',
-                'noteMoyenne' => 3.1415,
-            ]
-        );
-
-        $I->amOnPage('/');
-        $I->click('Flanc');
-        $I->seeResponseCodeIsSuccessful();
-        $I->seeCurrentRouteIs('app_details');
-
-    }
-
-    public function isOrderCorrect(ControllerTester $I): void
-    {
-        RecetteFactory::createSequence(
-            [
-                ['nomRecette' => 'Tarte',
-                    'tempsRecette' => new \DateTime('02:36:00'),
-                    'diffRecette' => 3,
-                    'instruction' => 'Le Lorem Ipsum est un texte employé dans la composition et la mise en page.',
-                    'description' => 'Une recette test',
-                    'noteMoyenne' => 2.16,
-                ],
-                ['nomRecette' => 'Flanc',
-                    'tempsRecette' => new \DateTime('02:36:00'),
-                    'diffRecette' => 3,
-                    'instruction' => 'Le Lorem Ipsum est un texte employé dans la composition et la mise en page.',
-                    'description' => 'Une recette test',
-                    'noteMoyenne' => 3.1415,
-                ],
-                ['nomRecette' => 'Gauffre',
-                    'tempsRecette' => new \DateTime('02:36:00'),
-                    'diffRecette' => 3,
-                    'instruction' => 'Le Lorem Ipsum est un texte employé dans la composition et la mise en page.',
-                    'description' => 'Une recette test',
-                    'noteMoyenne' => 3.1415,
-                ],
-            ]
-        );
-
-        $I->amOnPage('/');
-        $aLinkText = $I->grabMultiple('div#bloc_list > ul > li > a > div > span.text-xl.w-64.text-center');
-        //Test ordre décroissant sur la note, PUIS, alphabétique sur le nom
-        $I->assertEqualsIgnoringCase(['Flanc','Gauffre','Tarte'], $aLinkText, 'Les deux arrays ne sont pas égales');
+        $I->seeElement('#trending');
+        $I->see('Les mieux notées :', 'h1');
+        $I->seeElement('#bloc_list');
     }
 
 }
