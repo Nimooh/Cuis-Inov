3652ce53402815051db6e13c747643c66afe67fe
burl0005 <brice.burlot@etudiant.univ-reims.fr>
2024-01-06 14:53:59 +0100

    Correction des problèmes d'implémentation des favoris causés par la requete en queryBuilder

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -25,23 +25,25 @@ class HomeController extends AbstractController
         } else {
             $recettes = $repo->findAllOrderedWithoutMostTrending(0, $userId);
         }
-        //dump($recettes);
+        dump($recettes);
         return $this->render('home/index.html.twig', ['recettes' => $recettes, 'trending' => $trending]);
     }
 
     #[Route('/update', name: 'app_recette_update')]
-    public function updateDB(InteragirRepository $repo, #[MapQueryParameter(filter: \FILTER_VALIDATE_BOOLEAN)] ?bool $fav,
+    public function updateDB(InteragirRepository $repo, #[MapQueryParameter(filter: \FILTER_VALIDATE_INT)] ?int $fav,
                              #[MapQueryParameter(filter: \FILTER_VALIDATE_INT)] int $idRecette)
     {
         /** @var \App\Entity\Membre $user */
         $user = $this->getUser();
 
+        dump($fav);
+
         //Redirection vers la page de connexion, si l'utilisateur non-connecté se retrouve sur cette page
         if($user === null)
             return $this->redirectToRoute('app_login', [], 303);
         //Rajoute une nouvelle ligne dans la BD, si aucune interaction avant
-        if($fav === null)
-            $repo->insertDB($user->getId(), $idRecette);
+        if($fav === 3)
+            $repo->insertDB($user->getId(), $idRecette, 1);
         else
             $repo->updateDB($fav, $user->getId(), $idRecette);
 
