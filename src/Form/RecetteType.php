0cfa4427e1ad05731906f99da8eb144659118cd0
Antoine Terrier <antoine.terrier@etudiant.univ-reims.fr>
2024-01-06 18:58:22 +0100

    Correction du champs composers du formulaire de l'entité Recette

--- src/Form/RecetteType.php
+++ src/Form/RecetteType.php
@@ -3,14 +3,13 @@
 namespace App\Form;
 
 use App\Entity\CategorieRecette;
-use App\Entity\Ingredient;
 use App\Entity\Recette;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\DateIntervalType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -45,31 +44,24 @@ class RecetteType extends AbstractType
                     'empty_data' => '',
                 ]
             )
-            ->add('noteMoyenne', NumberType::class, [
-                    'empty_data' => '',
-                ]
-            )
             ->add('categoriesRecette', EntityType::class, [
                 'class' => CategorieRecette::class,
                 'choice_label' => 'nomCatRecette',
-                'required' => false,
                 'multiple' => true,
+                'expanded' => true,
                 'placeholder' => 'Catégories',
                 'query_builder' => function (EntityRepository $entityRepository) {
                     return $entityRepository->createQueryBuilder('c')
                         ->orderBy('c.nomCatRecette', 'ASC');
                 },
             ])
-            ->add('composers', EntityType::class, [
-                'class' => Ingredient::class,
-                'choice_label' => 'nomIngr',
+            ->add('composers', CollectionType::class, [
+                'entry_type' => ComposerType::class,
+                'entry_options' => ['label' => false],
                 'required' => false,
-                'multiple' => true,
-                'placeholder' => 'Ingredients',
-                'query_builder' => function (EntityRepository $entityRepository) {
-                    return $entityRepository->createQueryBuilder('i')
-                        ->orderBy('i.nomIngr', 'ASC');
-                },
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
             ])
         ;
     }
