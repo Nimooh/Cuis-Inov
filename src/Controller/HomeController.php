b542ca2e0e6077c938cfc13425d453b541cfdc32
Ruben <ruben.dunesme@gmail.com>
2024-01-07 17:36:09 +0100

    Modification du HomeController pour l'implementation des filtres

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,34 +16,61 @@ class HomeController extends AbstractController
     {
         /** @var \App\Entity\Membre $user */
         $user = $this->getUser();
-        $userId = $user ? $user->getId() : 0; //User par défault id 0
+        $userId = $user ? $user->getId() : 0; // User par défault id 0
+        if (!isset($_POST['search'])) {
+            $_POST['search'] = null;
+        }
+        if (!isset($_POST['difficulte'])) {
+            $_POST['difficulte'] = null;
+        }
+        if (!isset($_POST['temps'])) {
+            $_POST['temps'] = null;
+        }
+        if (!isset($_POST['note'])) {
+            $_POST['note'] = null;
+        }
+        if (!isset($_POST['ingredient_oui'])) {
+            $_POST['ingredient_oui'] = null;
+        }
+        if (!isset($_POST['ingredient_non'])) {
+            $_POST['ingredient_non'] = null;
+        }
+        if (!isset($_POST['categorie'])) {
+            $_POST['categorie'] = null;
+        }
+        if (!isset($_POST['allergene'])) {
+            $_POST['allergene'] = null;
+        }
 
         $trending = $repo->findMostTrending();
-        if(!empty($trending)) {
+        if (!empty($trending)) {
             $trendingId = $trending->getId();
-            $recettes = $repo->findAllOrderedWithoutMostTrending($trendingId, $userId);
+            $recettes = $repo->findAllOrderedWithoutMostTrending($trendingId, $userId, $_POST['search'], $_POST['difficulte'], $_POST['temps'], $_POST['note'], $_POST['ingredient_oui'], $_POST['ingredient_non'], $_POST['categorie'], $_POST['allergene']);
         } else {
-            $recettes = $repo->findAllOrderedWithoutMostTrending(0, $userId);
+            $recettes = $repo->findAllOrderedWithoutMostTrending(0, $userId, $_POST['search'], $_POST['difficulte'], $_POST['temps'], $_POST['note'], $_POST['ingredient_oui'], $_POST['ingredient_non'], $_POST['categorie'], $_POST['allergene']);
         }
-        //dump($recettes);
+
+        // dump($recettes);
         return $this->render('home/index.html.twig', ['recettes' => $recettes, 'trending' => $trending]);
     }
 
     #[Route('/update', name: 'app_recette_update')]
     public function updateDB(InteragirRepository $repo, #[MapQueryParameter(filter: \FILTER_VALIDATE_BOOLEAN)] ?bool $fav,
-                             #[MapQueryParameter(filter: \FILTER_VALIDATE_INT)] int $idRecette)
+        #[MapQueryParameter(filter: \FILTER_VALIDATE_INT)] int $idRecette)
     {
         /** @var \App\Entity\Membre $user */
         $user = $this->getUser();
 
-        //Redirection vers la page de connexion, si l'utilisateur non-connecté se retrouve sur cette page
-        if($user === null)
+        // Redirection vers la page de connexion, si l'utilisateur non-connecté se retrouve sur cette page
+        if (null === $user) {
             return $this->redirectToRoute('app_login', [], 303);
-        //Rajoute une nouvelle ligne dans la BD, si aucune interaction avant
-        if($fav === null)
+        }
+        // Rajoute une nouvelle ligne dans la BD, si aucune interaction avant
+        if (null === $fav) {
             $repo->insertDB($user->getId(), $idRecette);
-        else
+        } else {
             $repo->updateDB($fav, $user->getId(), $idRecette);
+        }
 
         return $this->redirectToRoute('app_home');
     }
