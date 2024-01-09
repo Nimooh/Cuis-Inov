76bda3a7df61a03c1fd2bed08b97d1bd5300b864
Steven Morlet <steven.morlet@etudiant.univ-reims.fr>
2024-01-09 17:42:20 +0100

    Changement route profil + refactor profil -> profile + recuperation id user connecte

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ProfilController extends AbstractController
+class ProfileController extends AbstractController
 {
-    #[Route('/profil', name: 'app_profil')]
+    #[Route('/profile', name: 'app_profile')]
     public function index(): Response
     {
-        return $this->render('profil/index.html.twig', [
-            'controller_name' => 'ProfilController',
-        ]);
+        $member = $this->getUser();
+
+        return $this->render('profile/index.html.twig', ['member' => $member]);
     }
 }
