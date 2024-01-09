46bfb968c8b9013ab5a48700a861edc6cf6c4f2f
Steven Morlet <steven.morlet@etudiant.univ-reims.fr>
2024-01-09 17:46:54 +0100

    Update Profil : ajout actions update et delete, avec templates

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -5,14 +5,32 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
 
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+    public function show(): Response
     {
         $member = $this->getUser();
 
         return $this->render('profile/index.html.twig', ['member' => $member]);
     }
+
+    #[Route('/profile/update', name: 'app_profile_update')]
+    public function update(): Response
+    {
+        $member = $this->getUser();
+
+        return $this->render('profile/update.html.twig', ['member' => $member]);
+    }
+
+    #[Route('/profile/delete', name: 'app_profile_delete')]
+    public function delete(): Response
+    {
+        $memberId = $this->getUser()->getId();
+
+        return $this->render('profile/delete.html.twig', ['memberId' => $memberId]);
+    }
 }
+
