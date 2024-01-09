05015590f5607f576d89a7e95083382479f316e5
Steven Morlet <steven.morlet@etudiant.univ-reims.fr>
2024-01-09 18:29:35 +0100

    Update form Profil : mdp non visible, changement en PasswordType + ajout requireds

--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -7,6 +7,8 @@ use App\Entity\Membre;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,11 +17,22 @@ class ProfileType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
-            ->add('nomMembre')
-            ->add('prnmMembre')
-            ->add('telMembre')
+            ->add('email', TextType::class, [
+                'required' => true,
+            ])
+            ->add('password', PasswordType::class, [
+                'hash_property_path' => 'password',
+                'mapped' => false,
+            ])
+            ->add('nomMembre', TextType::class, [
+                'required' => true,
+            ])
+            ->add('prnmMembre', TextType::class, [
+                'required' => true,
+            ])
+            ->add('telMembre', TextType::class, [
+                'required' => true,
+            ])
             ->add('allergenes', EntityType::class, [
                 'class' => Allergene::class,
                 'multiple' => true,
