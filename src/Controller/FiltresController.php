86224f8494dac5e2bb235f318bc4acf5a3a6e0f9
Ruben <ruben.dunesme@gmail.com>
2024-01-09 15:41:02 +0100

    Modification de l'implémentation des allergene dans la recherche

--- src/Controller/FiltresController.php
+++ src/Controller/FiltresController.php
@@ -7,18 +7,22 @@ use App\Repository\CategorieRecetteRepository;
 use App\Repository\IngredientRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class FiltresController extends AbstractController
 {
     public function filtre(IngredientRepository $ings, CategorieRecetteRepository $cats, AllergeneRepository $alls): Response
     {
+        /** @var \App\Entity\Membre $user */
+        $user = $this->getUser();
+
+        $user_allergenes =$user? $user->getAllergenes() : [];
+
         $ingredients = $ings->findAll();
         $categories = $cats->findAll();
         $allergenes = $alls->findAll();
 
         return $this->render('navbar/search.html.twig', [
-            'ingredients' => $ingredients, 'categories' => $categories, 'allergenes' => $allergenes,
+            'ingredients' => $ingredients, 'categories' => $categories, 'allergenes' => $allergenes, 'allergenes_user' => $user_allergenes,
         ]);
     }
 }
