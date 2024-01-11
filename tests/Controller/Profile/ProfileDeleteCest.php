279575f2dac2153d7ef01787fa72fb14912a2c7b
Steven Morlet <steven.morlet@etudiant.univ-reims.fr>
2024-01-11 04:08:12 +0100

    Ajout CEST Delete pour Profil

---
+++ tests/Controller/Profile/ProfileDeleteCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Tests\Controller\Profile;
+
+use App\Tests\Support\ControllerTester;
+
+class ProfileDeleteCest
+{
+    public function _before(ControllerTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ControllerTester $I)
+    {
+    }
+}
