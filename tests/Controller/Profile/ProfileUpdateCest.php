1176350e17c49b2eaf8b18cd216f32820ee6e8cd
Steven Morlet <steven.morlet@etudiant.univ-reims.fr>
2024-01-11 03:30:56 +0100

    Update CEST_Update : ajout test affichage form

--- tests/Controller/Profile/ProfileUpdateCest.php
+++ tests/Controller/Profile/ProfileUpdateCest.php
@@ -3,6 +3,7 @@
 
 namespace App\Tests\Controller\Profile;
 
+use App\Factory\MembreFactory;
 use App\Tests\Support\ControllerTester;
 
 class ProfileUpdateCest
@@ -11,10 +12,20 @@ class ProfileUpdateCest
     {
     }
 
-    // tests
     public function accessIsRestrictedToAuthenticatedUsers(ControllerTester $I): void
     {
         $I->amOnPage('/profile/update');
         $I->seeCurrentRouteIs('app_login');
+    }
+    public function isFormLoaded(ControllerTester $I): void
+    {
+        $membre = MembreFactory::createOne()->object();
+
+        $I->amLoggedInAs($membre);
+        $I->amOnPage('/profile/update');
+        $I->seeInTitle("Edition de ".$membre->getPrnmMembre()." ".$membre->getNomMembre());
+        $I->see("Mon Profil", 'h1');
+        $I->see("Mes Allergènes", 'h1');
+        $I->assertEquals($I->grabValueFrom('#profile_email'), $membre->getUserIdentifier());
     }
 }
