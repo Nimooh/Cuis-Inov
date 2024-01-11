fc8ea116e68d29527df65d3b5aa1aa73be0c5ec3
Steven Morlet <steven.morlet@etudiant.univ-reims.fr>
2024-01-11 03:15:42 +0100

    Update CEST_Update : ajout test acces restreint user connecte

---
+++ tests/Controller/Profile/ProfileUpdateCest.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Tests\Controller\Profile;
+
+use App\Tests\Support\ControllerTester;
+
+class ProfileUpdateCest
+{
+    public function _before(ControllerTester $I)
+    {
+    }
+
+    // tests
+    public function accessIsRestrictedToAuthenticatedUsers(ControllerTester $I): void
+    {
+        $I->amOnPage('/profile');
+        $I->seeCurrentRouteIs('app_login');
+    }
+}
