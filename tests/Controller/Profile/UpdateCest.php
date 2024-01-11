003922c6e37cde1c1a3afec33e46697ba13e0c03
Steven Morlet <steven.morlet@etudiant.univ-reims.fr>
2024-01-11 03:13:32 +0100

    Ajout CEST Profil action Update

---
+++ tests/Controller/Profile/UpdateCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Tests\Controller\Profile;
+
+use App\Tests\Support\ControllerTester;
+
+class UpdateCest
+{
+    public function _before(ControllerTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ControllerTester $I)
+    {
+    }
+}
