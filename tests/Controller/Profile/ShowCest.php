86e850f3811c064576d47917128397462e4ab27b
Steven Morlet <steven.morlet@etudiant.univ-reims.fr>
2024-01-11 02:26:25 +0100

    Ajout CEST_Show pour Profil

---
+++ tests/Controller/Profile/ShowCest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Tests\Controller\Profile;
+
+use App\Factory\MembreFactory;
+use App\Tests\Support\ControllerTester;
+
+class ShowCest
+{
+    public function _before(ControllerTester $I)
+    {
+    }
+}
